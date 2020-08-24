784391a24068e00611911a425089fb06e029e9c0
PenaltyDbl <2notrump@gmail.com>
2020-08-23 21:08:33 -0400

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -13,12 +13,12 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
+    #if test_config is None:
+    #    # load the instance config, if it exists, when not testing
+    #    app.config.from_pyfile("config.py", silent=True)
+    #else:
+    #    # load the test config if passed in
+    #    app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
